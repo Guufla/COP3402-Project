51b5a009c6071dbed58216b52a7f69085ffdd0b7
Guufla <liamabramov@gmail.com>
2024-09-16 19:19:15 -0400

    Changes1

---
+++ Hw1/machine.c
@@ -0,0 +1,5 @@
+#include "machine_types.h"
+#include "instruction.h"
+
+
+
---
+++ Hw1/machine_main.c
@@ -0,0 +1,15 @@
+#include "machine.c"
+
+// a size for the memory ( 2 ^ 1 6 words = 32K words )
+#define MEMORY_SIZE_IN_WORDS 32768
+static union mem_u {
+    word_type words[MEMORY_SIZE_IN_WORDS];
+    uword_type uwords[MEMORY_SIZE_IN_WORDS];
+    bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
+} memory;
+
+
+
+
+
+
