ec70242ff898a887119a2c418b450e3842f47a22
Guufla <liamabramov@gmail.com>
2024-09-20 10:48:47 -0400

    9/20/2024 some changes

--- Hw1/machine_main.c
+++ Hw1/machine_main.c
@@ -1,45 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "bof.h"
+#include "utilities.h"
 
-#include "machine.c"
 
-// a size for the memory ( 2 ^ 1 6 words = 32K words )
-#define MEMORY_SIZE_IN_WORDS 32768
-static union mem_u {
-    word_type words[MEMORY_SIZE_IN_WORDS];
-    uword_type uwords[MEMORY_SIZE_IN_WORDS];
-    bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
-} memory;
 
 int main(int argc, char *argv[]) {
 
-
-    // Represents the Registers
-    int gp = 0;
-    int sp = 0;
-    int fp = 0;
-    int r3 = 0;
-    int r4 = 0;
-    int r5 = 0;
-    int r6 = 0; 
-    int ra = 0;
-
-    int pc = 0;
-    int hi = 0;
-    int lo = 0;
-
-
-
     int pFlag = 0;
+    argc--;
+    argv++;
 
     for(int i = 1; i < argc; i++){
-        if(strcmp(argv[i],"-p") == 0){
+        if(argc == 2 && (argv[i],"-p") == 0){
             pFlag = 1;
         }
     }
 
+<<<<<<< Updated upstream
+=======
+
+    BOFFILE bf = bof_read_open(argv[0]);
+
+    machine_load(bf);
+
+>>>>>>> Stashed changes
     if (pFlag){
-        printf("Flag -p detected");
+        machine_print_loaded_program(stdout);
+	    return EXIT_SUCCESS;
     }
     else{
         printf("Flag -p not detected");
