c6dbd94d7efad0e16716fa0fa9fe8a40354b4503
Guufla <liamabramov@gmail.com>
2024-09-24 22:06:15 -0400

    Some functions for machine.c

--- machine.c
+++ machine.c
@@ -1,19 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <math.h>
 #include "machine_types.h"
-#include "machine_types.c"
-#include "instruction.c"
-#include "instruction.h"
-#include "bof.c"
-#include "bof.h"
-#include "regname.c"
+#include "machine.h"
 #include "regname.h"
-#include "utilities.c"
 #include "utilities.h"
-
-
-
 
 // a size for the memory ( 2 ^ 1 6 words = 32K words )
 #define MEMORY_SIZE_IN_WORDS 32768
+
+
 static union mem_u {
     word_type words[MEMORY_SIZE_IN_WORDS];
     uword_type uwords[MEMORY_SIZE_IN_WORDS];
@@ -24,3 +22,43 @@ static union gpr_u{
     word_type ints[NUM_REGISTERS];
     uword_type floats[NUM_REGISTERS];
 }GPR;
+
+static address_type PC;
+
+
+
+static void initialize(){
+    for(int j = 0; j < NUM_REGISTERS; j++){
+        GPR.ints[j] = 0;
+    }
+    \
+    for(int j = 0; j < MEMORY_SIZE_IN_WORDS; j++){
+        memory.words[j] = 0;
+    }
+}
+
+void machine_load(BOFFILE bf){
+
+}
+
+void machine_execute_instr(bin_instr_t bi){
+
+}
+
+static void print_global_data(FILE *out){
+
+}
+
+static void print_registers(FILE *out){
+
+}
+
+static void print_runtime_stack_AR(FILE *out){
+    
+}
+
+void machine_print_state(FILE *out){
+    print_registers(out);
+    print_global_data(out);
+    print_runtime_stack_AR(out);
+}
