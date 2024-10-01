f902b4d9fd463c3a015b5a54634f682315380c73
PetersJoseph <petersjoseph19@gmail.com>
2024-10-01 17:34:55 -0400

    change in view of registers (test)
    probably won't work but worth a shot

--- machine.c
+++ machine.c
@@ -40,7 +40,7 @@ static void initialize(){
         GPR.words[j] = 0;
         GPR.uwords[j] = 0;
     }
-    \
+    
     for(int j = 0; j < MEMORY_SIZE_IN_WORDS; j++){
         memory.words[j] = 0;
         memory.uwords[j] = 0;
@@ -55,8 +55,6 @@ void machine_load(BOFFILE bf){
 
 
 void machine_execute_instr(bin_instr_t bi){
-
-    PC = PC + BYTES_PER_WORD;
     
     instr_type it = instruction_type(bi);
     switch(it){
@@ -72,37 +70,44 @@ void machine_execute_instr(bin_instr_t bi){
                 }
                 case ADD_F: // Add
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = memory.words[GPR.words[SP]] + machine_types_formOffset(com.os);
+                    GPR.words[com.rt] = GPR.words[SP] + machine_types_formOffset(com.os);
+
                     break;
                 }
                 case SUB_F: // Subtract
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = memory.words[GPR.words[SP]] - machine_types_formOffset(com.os);
+                    GPR.words[com.rt] = GPR.words[SP] - machine_types_formOffset(com.os);
+
                     break;
                 }
                 case CPW_F: // Copy word
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = memory.words[GPR.words[com.rs]] + machine_types_formOffset(com.os);
+                    GPR.words[com.rt] = GPR.words[com.rs] + machine_types_formOffset(com.os);
+
                     break;
                 }
                 case AND_F: // Bitwise And
                 {
-                    memory.uwords[GPR.uwords[com.rt] + machine_types_formOffset(com.ot)] = memory.uwords[SP] && (memory.uwords[GPR.uwords[com.rs] + machine_types_formOffset(com.os)]);
+                    GPR.uwords[com.rt] = GPR.uwords[SP] & GPR.uwords[com.rs];
+
                     break;
                 }
                 case BOR_F: // Bitwise OR
                 {
-                    memory.uwords[GPR.uwords[com.rt] + machine_types_formOffset(com.ot)] = memory.uwords[SP] || (memory.uwords[GPR.uwords[com.rs] + machine_types_formOffset(com.os)]);
+                    GPR.uwords[com.rt] = GPR.uwords[SP] | GPR.uwords[com.rs];
+
                     break;
                 }
                 case NOR_F: // Not or
                 {   
-                    memory.uwords[GPR.uwords[com.rt] + machine_types_formOffset(com.ot)] = !memory.uwords[SP] || (memory.uwords[GPR.uwords[com.rs] + machine_types_formOffset(com.os)]);
+                    GPR.uwords[com.rt] = ~(GPR.uwords[SP] | GPR.uwords[com.rs]);
+
                     break;
                 }
                 case XOR_F: // Exclusive - or
                 {
-                    memory.uwords[GPR.uwords[com.rt] + machine_types_formOffset(com.ot)] = memory.uwords[SP] ^ (memory.uwords[GPR.uwords[com.rs] + machine_types_formOffset(com.os)]);
+                    GPR.uwords[com.rt] = GPR.uwords[SP] ^ GPR.uwords[com.rs];
+
                     break;
                 }
                 case LWR_F: // Load Word into register
@@ -112,22 +117,26 @@ void machine_execute_instr(bin_instr_t bi){
                 }
                 case SWR_F: // Store word in register
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = GPR.words[com.rs];
+                    memory.words[GPR.words[com.rt]] = GPR.words[com.rs];
+
                     break;
                 }
                 case SCA_F: // Store Computed address
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = GPR.words[com.rs] + machine_types_formOffset(com.os);
+                    GPR.words[com.rt] = GPR.words[com.rs] + machine_types_formOffset(com.os);
+
                     break;
                 }
                 case LWI_F: // Load word Indirect
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = memory.words[memory.words[GPR.words[com.rs] + machine_types_formOffset(com.os)]];
+                    GPR.words[com.rt] = memory.words[memory.words[GPR.words[com.rs] + machine_types_formOffset(com.os)]];
+
                     break;
                 }
                 case NEG_F: // Negate
                 {
-                    memory.words[GPR.words[com.rt] + machine_types_formOffset(com.ot)] = -memory.words[GPR.words[com.rs] + machine_types_formOffset(com.os)];
+                    GPR.words[com.rt] = -GPR.words[com.rs];
+
                     break;
                 }
 
@@ -141,7 +150,8 @@ void machine_execute_instr(bin_instr_t bi){
             {
                 case LIT_F: // Literal (Load)
                 {
-                    memory.words[GPR.words[ocom.reg] + machine_types_formOffset(ocom.offset)] = machine_types_sgnExt(ocom.arg);
+                    GPR.words[ocom.reg] = machine_types_sgnExt(ocom.arg);
+
                     break;
                 }
                 case ARI_F: // Add register immedidiate
@@ -161,34 +171,39 @@ void machine_execute_instr(bin_instr_t bi){
                 }
                 case DIV_F: // Divide (remainder in HI, quotient in LO)
                 {
-                    HI = memory.words[GPR.words[SP]] % (memory.words[GPR.words[ocom.reg]] + machine_types_sgnExt(ocom.arg));
+                    HI = GPR.words[SP] % (GPR.words[ocom.reg] + machine_types_sgnExt(ocom.arg));
+                    LO = GPR.words[SP] / (GPR.words[ocom.reg] + machine_types_sgnExt(ocom.arg));
 
-                    LO = memory.words[GPR.words[SP]] / (memory.words[GPR.words[ocom.reg]] + machine_types_sgnExt(ocom.arg));
                     break;
                 }
                 case CFHI_F: // Copy from HI
                 {
-                    memory.words[GPR.words[ocom.reg] + machine_types_formOffset(ocom.offset)] = HI;
+                    GPR.words[ocom.reg] = HI;
+
                     break;
                 }
                 case CFLO_F: // Copy from LO
                 {
-                    memory.words[GPR.words[ocom.reg] + machine_types_formOffset(ocom.offset)] = LO;
+                    GPR.words[ocom.reg] = LO;
+
                     break;
                 }
                 case SLL_F: // Shift left logical
                 {
-                    memory.uwords[GPR.uwords[ocom.reg] + machine_types_formOffset(ocom.offset)] = memory.uwords[GPR.uwords[SP]] << ocom.arg;
+                    GPR.uwords[ocom.reg] = GPR.uwords[SP] << ocom.arg;
+
                     break;
                 }
                 case SRL_F: // Shift right logical
                 {
-                    memory.uwords[GPR.uwords[ocom.reg] + machine_types_formOffset(ocom.offset)] = memory.uwords[GPR.uwords[SP]] >> ocom.arg;
+                    GPR.uwords[ocom.reg] = GPR.uwords[SP] >> ocom.arg;
+
                     break;
                 }
                 case JMP_F: // Jump
                 {
-                    PC = memory.uwords[GPR.uwords[ocom.offset] + machine_types_formOffset(ocom.offset)];
+                    PC = GPR.uwords[ocom.reg];
+
                     break;
                 }
                 case CSI_F: // Call subroutine Indirectly
@@ -214,12 +229,14 @@ void machine_execute_instr(bin_instr_t bi){
                         }
                         case print_str_sc:
                         {
-                            memory.words[GPR.words[SP]] = printf("%d" , &memory.words[GPR.words[bi.syscall.reg] + machine_types_formOffset(bi.syscall.offset)]);
+                            printf("%d", GPR.words[bi.syscall.reg] + machine_types_formOffset(bi.syscall.offset));
+
                             break;
                         }
                         case print_char_sc:
                         {
-                            memory.words[GPR.words[SP]] = fputc(memory.words[GPR.words[bi.syscall.reg] + machine_types_formOffset(bi.syscall.offset)] , stdout);
+                            fputc(GPR.words[bi.syscall.reg] + machine_types_formOffset(bi.syscall.offset), stdout);
+
                             break;
                         }
                         case read_char_sc:
@@ -255,72 +272,78 @@ void machine_execute_instr(bin_instr_t bi){
             {
                 case ADDI_O: // Add immediate
                 {
-                    memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)] = (memory.words[GPR.words[im.reg]] + machine_types_formOffset(im.offset)) + machine_types_sgnExt(im.immed);
+                    GPR.words[im.reg] = GPR.words[im.reg] + machine_types_sgnExt(im.immed);
+
                     break;
                 }
                 case ANDI_O: // Bitwise and immediate
                 {  
-                    memory.uwords[GPR.uwords[im.reg] + machine_types_formOffset(im.offset)] = (memory.uwords[GPR.uwords[im.reg]] + machine_types_formOffset(im.offset)) && machine_types_sgnExt(im.immed);
+                    GPR.uwords[im.reg] = GPR.uwords[im.reg] & machine_types_sgnExt(im.immed);
+
                     break;
                 }
                 case BORI_O: // Bitwise or immediate
                 {
-                    memory.uwords[GPR.uwords[im.reg] + machine_types_formOffset(im.offset)] = (memory.uwords[GPR.uwords[im.reg]] + machine_types_formOffset(im.offset)) || machine_types_sgnExt(im.immed);
+                    GPR.uwords[im.reg] = GPR.uwords[im.reg] | machine_types_sgnExt(im.immed);
+
                     break;
                 }
                 case NORI_O: // Bitwise or not immediate
                 {
-                    memory.uwords[GPR.uwords[im.reg] + machine_types_formOffset(im.offset)] = !(memory.uwords[GPR.uwords[im.reg]] + machine_types_formOffset(im.offset)) || machine_types_sgnExt(im.immed);
+                    GPR.uwords[im.reg] = ~(GPR.uwords[im.reg] | machine_types_sgnExt(im.immed));
+
                     break;
                 }
                 case XORI_O: // Bitwise Exclusive-Or immediate
                 {
-                    memory.uwords[GPR.uwords[im.reg] + machine_types_formOffset(im.offset)] = (memory.uwords[GPR.uwords[im.reg]] + machine_types_formOffset(im.offset)) ^ machine_types_sgnExt(im.immed);
+                    GPR.uwords[im.reg] = GPR.uwords[im.reg] ^ machine_types_sgnExt(im.immed);
+
                     break;
                 }
                 case BEQ_O: // Branch on equal
                 {
-                    if(memory.words[GPR.words[SP]] == memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)]){
+                    if(GPR.words[SP] == GPR.words[im.reg]) {
                         PC = (PC - 1) + machine_types_formOffset(im.immed);
-                    }
+                        }
+
                     break;
-                }
+                    }
+                
                 case BGEZ_O: // Branch >= 0
                 {
-                    if(memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)] >= 0){
+                    if(GPR.words[im.reg] >= 0) {
                         PC = (PC - 1) + machine_types_formOffset(im.immed);
                     }
                     break;
                 }
                 case BGTZ_O: // Branch > 0
                 {
-                    if(memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)] > 0){
+                    if(GPR.words[im.reg] > 0) {
                         PC = (PC - 1) + machine_types_formOffset(im.immed);
                     }
                     break;
                 }
                 case BLEZ_O:
                 {
-                    if(memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)] <= 0){
+                    if(GPR.words[im.reg] <= 0) {
                         PC = (PC - 1) + machine_types_formOffset(im.immed);
                     }
                     break;
                 }
                 case BLTZ_O:
                 {
-                    if(memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)] < 0){
+                    if(GPR.words[im.reg] < 0) {
                         PC = (PC - 1) + machine_types_formOffset(im.immed);
                     }
                     break;
                 }
                 case BNE_O:
                 {
-                    if(memory.words[GPR.words[SP]] != memory.words[GPR.words[im.reg] + machine_types_formOffset(im.offset)]){
+                    if(GPR.words[SP] != GPR.words[im.reg]) {
                         PC = (PC - 1) + machine_types_formOffset(im.immed);
                     }
                     break;
                 }
-                
 
             }
             break;
@@ -354,6 +377,7 @@ void machine_execute_instr(bin_instr_t bi){
         
         // DO NOT FORGET BREAKS AT THE END OF EACH CASE !!!!!
     }
+    PC = PC + BYTES_PER_WORD;
 }
 
 static void print_global_data(FILE *out){
--- machine.h
+++ machine.h
@@ -1,13 +1,33 @@
 
 #ifndef _MACHINE_H
-#endif
+
 #define _MACHINE_H
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include "regname.h"
 #include "machine_types.h"
 #include "bof.h"
 #include "instruction.h"
 
+// Define external variables so they can be used across files
+extern union gpr_u {
+    word_type words[NUM_REGISTERS];
+    uword_type uwords[NUM_REGISTERS];
+} GPR;
+
+extern address_type PC;
+
+extern int HI;
+extern int LO;
+
+extern bool tracing;
+
+// Function declarations
+void initialize();
+void machine_execute_instr(bin_instr_t bi);
+
 #define MAX_STACK_HEIGHT 2048
+
+#endif
 
